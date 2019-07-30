e75afe0b61ebbb7bd8606426644e7e949c674f27
Gian Maria Ricci <alkampfer@nablasoft.com>
2019-07-30 21:42:15 +0200

    Added basic unit test project

--- CoreBasicSample/src/MyWonderfulApp.Tests/MyMathTests.cs
+++ CoreBasicSample/src/MyWonderfulApp.Tests/MyMathTests.cs
@@ -1,8 +1,24 @@
-﻿using System;
+﻿using MyWonderfulApp.Data;
+using NUnit.Framework;
+using System;
 
 namespace MyWonderfulApp.Tests
 {
-    public class Class1
+    [TestFixture]
+    public class MyMathTests
     {
+        private MyMath sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            sut = new MyMath();
+        }
+
+        [Test]
+        public void Basic_add()
+        {
+            Assert.That(sut.Add(1, 3), Is.EqualTo(4));
+        }
     }
 }
