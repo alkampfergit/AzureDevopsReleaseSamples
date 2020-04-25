2ee6dd42e848b68bdb31c617e40a4cd6c9b9ba44
Gian Maria Ricci <alkampfer@nablasoft.com>
2020-04-25 18:42:50 +0200

    Small refactor

--- CoreBasicSample/src/MyWonderfulApp.Service/Models/V1/GetCustomer2Dto.cs
+++ CoreBasicSample/src/MyWonderfulApp.Service/Models/V1/GetCustomer2Dto.cs
@@ -1,15 +1,12 @@
 ﻿using MyWonderfulApp.Data.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyWonderfulApp.Service.Models.V1
 {
-public class GetCustomer2Dto
-{
-    public CustomerId CustomerId { get; set; }
+    public class GetCustomer2Dto
+    {
+        public CustomerId CustomerId { get; set; }
 
-    public Int32 OtherParam { get; set; }
-}
+        public Int32 OtherParam { get; set; }
+    }
 }
